d12a19c16ce62fe0289056e7aabc72f75f444cd4
Logan Scott <logantscott@gmail.com>
2020-06-09 06:47:40 -0600

    first route test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,14 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
-
+describe('check route responses', () => {
+  // <html><body><h1>red</h1></body></html>
+  it('root', () => {
+    return request(app)
+      .get('/')
+      .send()
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
 });
